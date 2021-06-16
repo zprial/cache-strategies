57d4f1ae4afb240009034abbda611eb86e214626
zprial <zhangtong@haoduo.vip>
2021-06-16 17:58:50 +0800

    feat: 新增内存缓存

---
+++ src/adapters/memory.ts
@@ -0,0 +1,32 @@
+import { LRUMap } from "lru_map";
+import { Adapter } from "../types";
+import { validateKey } from "../validator";
+
+export class MemoryAdapter extends LRUMap<string, any> {
+  type = "memoryAdapter";
+  async getItem(key: string): Promise<any> {
+    if (!validateKey(key, "getItem")) {
+      return null;
+    }
+    const result = this.get(key);
+    if (result) {
+      return result;
+    }
+    return null;
+  }
+  async setItem(key: string, val: any) {
+    if (!validateKey(key, "setItem")) {
+      return;
+    }
+    return this.set(key, val);
+  }
+  async removeItem(key: string) {
+    if (!validateKey(key, "removeItem")) {
+      return;
+    }
+    return this.delete(key);
+  }
+}
+
+const memoryAdapter = new MemoryAdapter(Number.MAX_SAFE_INTEGER);
+export default memoryAdapter;
---
+++ test/adapters/memory.test.ts
@@ -0,0 +1,33 @@
+import memoryAdapter from "../../src/adapters/memory";
+
+describe("memoryAdapter接口测试:", () => {
+  beforeAll(() => {
+    console.error = () => {}
+  });
+
+  test("如果key不存在，getItem 返回null:", async () => {
+    const result = await memoryAdapter.getItem("abc");
+    expect(result).toBe(null);
+  });
+
+  test("如果key存在，getItem 返回对应值:", async () => {
+    await memoryAdapter.setItem("abc", 2333);
+    const result = await memoryAdapter.getItem("abc");
+    expect(result).toEqual(2333);
+  });
+
+  test("如果key不合法，memoryAdapter 各个api将不会处理:", async () => {
+    // @ts-ignore
+    const res1 = await memoryAdapter.getItem(123);
+    expect(res1).toBe(null);
+    // @ts-ignore
+    const res2 = await memoryAdapter.setItem(123);
+    expect(res2).toBe(undefined);
+    // @ts-ignore
+    const res3 = await memoryAdapter.removeItem(123);
+    expect(res3).toBe(undefined);
+    // @ts-ignore
+    const res4 = await memoryAdapter.removeItem("abc");
+    expect(res4).toBe(undefined);
+  });
+});
